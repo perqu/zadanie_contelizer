7fd454b8e8cb32b1fb1179a94764b80e0ba6f5d8
Paweł Perenc <57562561+perqu@users.noreply.github.com>
2024-04-23 18:37:42 +0200

    fix bug

--- pesel_validator/forms.py
+++ pesel_validator/forms.py
@@ -5,10 +5,13 @@ from .utils import check_control_digit
 class PeselField(forms.Field):
     def validate(self, value):
         length = len(str(value))
+        if not value.isnumeric():
+            raise ValidationError('PESEL number may only consist of digits')
         if length != 11:
             raise ValidationError(f'PESEL number must have 11 digits, you have {length}')
         if not check_control_digit(str(value)):
             raise ValidationError('PESEL number is incorrect')
+
 
 class PeselForm(forms.Form):
     pesel_number = PeselField(
